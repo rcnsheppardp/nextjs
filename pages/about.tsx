d5c36e9fd42c36d2a958b639c12684d156de0f4b
Peter Sheppard <peter.sheppard@rcn.org.uk>
2021-12-06 00:07:58 +0000

    Components and stuff

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/header";
+
+export default function About() {
+  return (
+    <main>
+      <Header />
+      About me
+    </main>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,28 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import { Header } from "../components/header";
+import { fetchPets } from "../helpers/fetchPets";
+import { helloDevJam, helloWorld } from "../helpers/helloWorld";
+
 export default function Home() {
-  console.log("Hi");
-  return <main>Hello, World! testing</main>;
+  //useState();
+  //const pets = fetchPets();
+  //console.log(pets);
+
+  //console.log(petNames);
+
+  return (
+    <main>
+      <Header />
+      <h1>Hello, World!</h1>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <ul>
+        {/* {pets.map((pet) => (
+          <li key={pet.id}>{pet.name}</li>
+        ))} */}
+      </ul>
+    </main>
+  );
 }
